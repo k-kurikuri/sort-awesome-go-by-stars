88f7999c01b2fac37bb6a97f3da8fc5f00168fdf
Kenji.Kurisaki <kurisaki@liquidinc.asia>
2019-03-20 15:35:35 +0900

    Update tablewriter to simpletable

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/alexeyco/simpletable"
 	"github.com/gocolly/colly"
 	"github.com/k-kurikuri/sort-awesome-go-by-stars/model"
-	"github.com/olekukonko/tablewriter"
 )
 
 const (
@@ -23,10 +23,9 @@ const (
 )
 
 var (
-	c            = colly.NewCollector()
-	r            = regexp.MustCompile(targetGithubURL)
-	packageMap   = make(map[string]model.Repositories)
-	tableHeaders = []string{"star", "package_url", "description"}
+	c          = colly.NewCollector()
+	r          = regexp.MustCompile(targetGithubURL)
+	packageMap = make(map[string]model.Repositories)
 )
 
 func main() {
@@ -112,16 +111,36 @@ func realMain() int {
 			topN = repositories[0:]
 		}
 
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader(tableHeaders)
+		table := simpletable.New()
+		table.Header = &simpletable.Header{
+			Cells: []*simpletable.Cell{
+				{Align: simpletable.AlignCenter, Text: "STAR"},
+				{Align: simpletable.AlignCenter, Text: "PACKAGE_URL"},
+				{Align: simpletable.AlignCenter, Text: "DESCRIPTION"},
+			},
+		}
+
 		for _, repo := range topN {
-			table.Append([]string{fmt.Sprintf("%d", repo.Star), repo.URL, repo.Description})
+			r := []*simpletable.Cell{
+				{Text: fmt.Sprintf("%d", repo.Star)},
+				{Text: repo.URL},
+				{Text: repo.Description},
+			}
+
+			table.Body.Cells = append(table.Body.Cells, r)
+		}
+
+		table.Footer = &simpletable.Footer{
+			Cells: []*simpletable.Cell{
+				{},
+				{},
+				{Align: simpletable.AlignRight, Text: k},
+			},
 		}
 
 		fmt.Println()
-		table.SetCaption(true, k)
-		table.Render()
-		fmt.Println()
+		table.SetStyle(simpletable.StyleUnicode)
+		table.Println()
 	}
 
 	return 0
