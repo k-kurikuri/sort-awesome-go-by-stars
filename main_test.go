cc7aee2be1dcdba935e20d5c94e848cf0160dd31
k-kurikuri <kurimixgoo@gmail.com>
2019-06-14 20:05:42 +0900

    tekitou

---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Sample(t *testing.T) {
+	t.Run("test sample", func(t *testing.T) {
+		assert.True(t, true)
+	})
+}
